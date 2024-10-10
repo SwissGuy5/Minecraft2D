acc4e9c5fdfb8629e888b089f102a4fbe9616388
szutciek <szutermaciek@gmail.com>
2024-10-10 11:59:46 +0200

    The tiles should be nice now

--- Graphics/TerrainRenderer.java
+++ Graphics/TerrainRenderer.java
@@ -1,7 +1,13 @@
 package Graphics;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import Terrain.*;
+
 
 public class TerrainRenderer extends JPanel {
     final int TILE_SIZE = 12;
@@ -12,40 +18,37 @@ public class TerrainRenderer extends JPanel {
     final Color WATER = new Color(28, 129, 212);
 
     private Terrain cachedTerrain;
+    private Map<Byte, BufferedImage> tileSprites;
 
     public TerrainRenderer(Terrain cachedTerrain) {
         this.cachedTerrain = cachedTerrain;
-
         this.setBackground(SKY);
+        loadSprites();
+    }
+
+    private void loadSprites() {
+        tileSprites = new HashMap<>();
+        try {
+        for (byte i = 0; i < (byte) 89; i++) {
+            String fileName = "./Assets/Tiles/texture-" + i + ".png";
+            tileSprites.put((byte) i, ImageIO.read(getClass().getResource(fileName)));
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     void drawTile(Graphics g, byte type, byte x, byte y) {
-        Graphics2D g2 = (Graphics2D) g;
-
-        switch (type) {
-            case 0:
-                g2.setColor(SKY);
-                break;
-            case 1:
-                g2.setColor(DIRT);
-                break;
-            case 2:
-                g2.setColor(STONE);
-                break;
-            case 3:
-                g2.setColor(WATER);
-                break;
-            default:
-                return;
+        BufferedImage sprite = tileSprites.get(type);
+        if (sprite != null) {
+            g.drawImage(sprite, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
         }
-        g2.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
 
     void drawChunk(Graphics g, Chunk chunk) {
         for (byte y = 0; y < Chunk.CHUNK_HEIGHT; y++) {
-            // System.out.println(Chunk.CHUNK_HEIGHT - y);
             for (byte x = 0; x < Chunk.CHUNK_WIDTH; x++) {
-                drawTile(g, chunk.getTile(x, y), x, (byte)(Chunk.CHUNK_HEIGHT - y - 1));
+                drawTile(g, chunk.getTile(x, y), x, (byte) (Chunk.CHUNK_HEIGHT - y - 1));
             }
         }
     }
