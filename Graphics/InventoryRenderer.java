5974ffa849636df98a3336e8969b51c3058c352b
szutciek <szutermaciek@gmail.com>
2024-10-27 23:42:51 +0100

    More stuff

--- Graphics/InventoryRenderer.java
+++ Graphics/InventoryRenderer.java
@@ -7,19 +7,24 @@ import Terrain.FileHandler;
 import Objects.Inventory;
 
 import java.awt.image.BufferedImage;
-// import javax.swing.BorderFactory;
-import javax.swing.*;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
 
+/**
+ * The renderer for the inventory.
+ */
 public class InventoryRenderer extends JPanel {
     private final int framePixelSize = 72;
     private final int gapLength = 5;
     private BufferedImage frameImg = FileHandler.getBufferedImage("./Assets/InventoryFrame.png");
-    private JLabel[] frames = new JLabel[Inventory.inventorySize];
+    private JLabel[] frames = new JLabel[Inventory.INV_SIZE];
     private Inventory inventory;
 
+    /**
+     * Creates a new inventory renderer.
+     * @param inventory The inventory object.
+     */
     public InventoryRenderer(Inventory inventory) {
         this.inventory = inventory;
         
@@ -30,28 +35,39 @@ public class InventoryRenderer extends JPanel {
 
         // this.setOpaque(false);
         this.setBackground(new Color(20, 20, 20, 150));
-        this.setBounds(Renderer.windowWidth / 2 - (framePixelSize + gapLength) * Inventory.inventorySize / 2, Renderer.windowHeight - 170 + gapLength, (framePixelSize + gapLength) * Inventory.inventorySize, framePixelSize + gapLength * 2);
+        this.setBounds(Renderer.windowWidth / 2 - (framePixelSize + gapLength)
+            * Inventory.INV_SIZE / 2, Renderer.windowHeight - 170 + gapLength, 
+            (framePixelSize + gapLength) * Inventory.INV_SIZE, framePixelSize + gapLength * 2);
         this.setVisible(true);
         
-        for (int i = 0; i < Inventory.inventorySize; i++) {
-            Image img = frameImg.getScaledInstance(framePixelSize, framePixelSize, Image.SCALE_SMOOTH);
+        for (int i = 0; i < Inventory.INV_SIZE; i++) {
+            Image img = frameImg.getScaledInstance(framePixelSize, 
+                framePixelSize, Image.SCALE_SMOOTH);
             JLabel frame = new JLabel(new ImageIcon(img), JLabel.CENTER);
             frames[i] = frame;
             this.add(frame);
         }
     }
 
+    /**
+     * Renders the inventory.
+     * @param g The graphics object.
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        Graphics2D g2d = (Graphics2D)g;
+        Graphics2D g2d = (Graphics2D) g;
         Point p = frames[inventory.currentlySelected].getLocation();
         g2d.setStroke(new BasicStroke(gapLength + 2));
         g2d.setColor(new Color(255, 255, 255, 150));
         g2d.drawRect(p.x, p.y, framePixelSize, framePixelSize);
     }
 
+    /**
+     * Paints the inventory items.
+     * @param g The graphics object.
+     */
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -60,11 +76,14 @@ public class InventoryRenderer extends JPanel {
 
         for (int i = 0; i < inventory.items.length; i++) {
             byte tileType = inventory.items[i];
-            if (tileType == 0) continue;
+            if (tileType == 0) {
+                continue;
+            }
             BufferedImage sprite = TerrainRenderer.tileSprites.get(tileType);
             if (sprite != null) {
                 p = frames[i].getLocation();
-                g.drawImage(sprite, p.x + framePixelSize / 4, p.y + framePixelSize / 5, 36, 36, null);
+                g.drawImage(sprite, p.x + framePixelSize / 4, p.y + framePixelSize / 5, 
+                    36, 36, null);
             }
         }
     }
